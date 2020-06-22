6b713cbb43e6b121c21811374e95b9081233369b
gixxerblade <sdclarkie@gmail.com>
2020-06-22 09:45:06 -0400

    update readme

--- Javascript/test.js
+++
@@ -1,5 +0,0 @@
-const KEY = Symbol(); // create new Symbol
-let object = {}; // create empty object
-object[KEY] = "lol"; // add Symbol to object
-console.log(object[KEY]); // "lol"
-console.log(object); // {Symbol(): "lol"}
